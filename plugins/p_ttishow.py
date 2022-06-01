b15d1015db8b928937c6770ae79eaa6d4bc8368e
cinexfilter <106576204+cinexfilter@users.noreply.github.com>
2022-06-01 20:20:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🍿 WEB SITE 🍿', url='https://cinesubz.com/'),
+            InlineKeyboardButton('📢 Channel', url='https://t.me/cinesubz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
