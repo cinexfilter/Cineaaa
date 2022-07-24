014afb16e78f724bcf6c13dad13e3e5dd5be8180
cinexfilter <106576204+cinexfilter@users.noreply.github.com>
2022-07-24 20:27:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -485,7 +485,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.AUTOFILTER_TXT,
-            reply_markup=reply_markup,
+            reply_markup=reply_markup, 
             parse_mode='html'
         )
     elif query.data == "coct":
